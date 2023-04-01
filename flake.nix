26cb25c28cafd06dca5df5fa67fcb2fc76b2c0fa
GabrielPK <gabrielkralik@outlook.com>
2023-04-01 00:27:03 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A Haskell API for OpenAI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Import the Haskell environment from nixpkgs
+        haskellEnv = pkgs.haskellPackages.developPackage {
+          root = ./.;
+          name = "openai-api";
+        };
+      in
+      {
+        devShell = haskellEnv.shellFor {
+          # Add any additional packages you need for development here
+          buildInputs = with pkgs; [
+            cabal-install
+            ghc
+            git
+          ];
+        };
+      }
+    );
+}
